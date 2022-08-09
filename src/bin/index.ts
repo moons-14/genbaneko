64afcaeed2714dc79380aa64c3666ba9261cf127
eggplants <w10776e8w@yahoo.co.jp>
2022-08-10 03:08:14 +0900

    imprement cli

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,2 +1,37 @@
-import { genbaneko } from '..';
-genbaneko();
+import { Command } from 'commander';
+import { mGenbaneko } from '..';
+
+const CLI_VERSION = '0.0.0';
+
+/**
+ * Run the command line interface program.
+ * @param argv process.argv.
+ */
+const cli = (argv: string[]) => {
+  const program = new Command();
+
+  program
+    .name('genbaneko')
+    .description(`Let's display "Yoshi!" on the console.`)
+    .version(CLI_VERSION);
+
+  program
+    .command('say')
+    .description('Genbaneko say something')
+    .argument('<string>', 'serif')
+    .action((serif) => {
+      mGenbaneko.say(serif);
+    });
+
+  program
+    .command('think')
+    .description('Genbaneko think something')
+    .argument('<string>', 'thought')
+    .action((thought) => {
+      mGenbaneko.think(thought);
+    });
+
+  program.parse(argv);
+};
+
+cli(process.argv);
