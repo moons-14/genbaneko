2353a217b9a3d81352ed03c1f42a66ea84b22dbe
eggplants <w10776e8w@yahoo.co.jp>
2022-08-10 03:09:41 +0900

    build

--- dist/bin/index.js
+++ dist/bin/index.js
@@ -1,4 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const commander_1 = require("commander");
 const __1 = require("..");
-(0, __1.genbaneko)();
+const CLI_VERSION = '0.0.0';
+/**
+ * Run the command line interface program.
+ * @param argv process.argv.
+ */
+const cli = (argv) => {
+    const program = new commander_1.Command();
+    program
+        .name('genbaneko')
+        .description(`Let's display "Yoshi!" on the console.`)
+        .version(CLI_VERSION);
+    program
+        .command('say')
+        .description('Genbaneko say something')
+        .argument('<string>', 'serif')
+        .action((serif) => {
+        __1.mGenbaneko.say(serif);
+    });
+    program
+        .command('think')
+        .description('Genbaneko think something')
+        .argument('<string>', 'thought')
+        .action((thought) => {
+        __1.mGenbaneko.think(thought);
+    });
+    program.parse(argv);
+};
+cli(process.argv);
